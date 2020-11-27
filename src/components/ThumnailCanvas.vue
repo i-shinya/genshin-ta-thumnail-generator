b1809f449503e95b8beb0407c0828083fd636597
i-shinya <sora0376@gmail.com>
2020-11-27 17:29:24 +0900

    first commit.

---
+++ src/components/ThumnailCanvas.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="thumnail-canvas">
+    サムネイルを表示
+    <v-btn color="success">ボタンのラベル</v-btn>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class ThumnailCanvas extends Vue {
+  @Prop() private msg!: string;
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <ThumnailCanvas />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import ThumnailCanvas from "@/components/ThumnailCanvas.vue";
+
+export default {
+  name: "Home",
+  components: {
+    ThumnailCanvas
+  }
+};
+</script>
