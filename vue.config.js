a4c994e8c38123caefd067be89ee00ecbf55ca4a
i-shinya <sora0376@gmail.com>
2020-12-02 03:37:01 +0900

    chore: remove pwa settings and rebuild.

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,15 @@
+const path = require('path');
+const vueSrc = './src';
+
 module.exports = {
   transpileDependencies: ["vuetify"],
-  publicPath: './',
-  outputDir: 'docs/',
+  configureWebpack: {
+    devtool: 'source-map',
+    resolve: {
+      alias: {
+        '@': path.join(__dirname, vueSrc),
+      },
+    },
+  },
+  productionSourceMap: false,
 };
