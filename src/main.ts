835867749ef22918f23c9405040271f9624a766b
i-shinya <sora0376@gmail.com>
2020-12-07 03:09:41 +0900

    chore: add google analytics settings.

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,18 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import vuetify from "./plugins/vuetify";
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import vuetify from './plugins/vuetify';
+import VueAnalytics from 'vue-analytics';
 
 Vue.config.productionTip = false;
+
+// Google Analyticsの設定
+Vue.use(VueAnalytics, {
+  id: 'G-QNPBBQMLHK'
+});
 
 new Vue({
   router,
   vuetify,
   render: h => h(App)
-}).$mount("#app");
+}).$mount('#app');
