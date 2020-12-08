6e8316109c962abc0742a6305f70f1af194783ff
i-shinya <sora0376@gmail.com>
2020-12-09 00:21:10 +0900

    chore: change vue-analytics to vue-gtag. rebuild.

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,14 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import vuetify from './plugins/vuetify';
-import VueAnalytics from 'vue-analytics';
+import VueGtag from 'vue-gtag';
 
 Vue.config.productionTip = false;
 
 // Google Analyticsの設定
-Vue.use(VueAnalytics, {
-  id: 'G-QNPBBQMLHK'
-});
+Vue.use(VueGtag, {
+  config: {id: 'G-QNPBBQMLHK'}
+}, router);
 
 new Vue({
   router,
